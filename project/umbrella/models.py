30429fc7683247ec544fe7edc199fe9db5905191
foxlollato <vitor.lollato@gmail.com>
2019-11-20 17:38:28 -0300

    rem auto_created attribute

--- project/umbrella/models.py
+++ project/umbrella/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 #simple value object model to store valuable information from api response
 class WeatherVO(models.Model):
-    dt = models.BigIntegerField(primary_key=True, auto_created=False)
+    dt = models.BigIntegerField(primary_key=True)
     when = models.DateTimeField()
     humidity = models.IntegerField()
     umbrella = models.BooleanField()
